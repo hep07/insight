059a8b2d884504d824544719514392042c8d29a3
junxie <kylinorange@gmail.com>
2018-01-29 13:07:47 -0800

    delete 'multiple'

--- forecaster/setup.py
+++ forecaster/setup.py
@@ -2,10 +2,16 @@ from setuptools import setup
 
 setup(name='forecaster',
       version='0.1',
-      description='multiple time series forecasting',
+      description='mtime series forecasting',
       url='https://github.com/kylinorange/insight/tree/master/forecaster',
       author='Jun Xie',
       author_email='kylinorange@gmail.com',
       license='BSD',
       packages=['forecaster'],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'tensorflow',
+          'sklearn',
+      ],
       zip_safe=False)
